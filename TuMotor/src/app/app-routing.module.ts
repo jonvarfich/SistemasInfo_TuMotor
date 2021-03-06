104114bcf51246d5b2005bc5020cd660a415832c
jonvarfich <jonvarfich@gmail.com>
2021-03-06 14:14:35 -0400

    home changes

--- TuMotor/src/app/app-routing.module.ts
+++ TuMotor/src/app/app-routing.module.ts
@@ -2,7 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+
+  {
+    path:'',
+    redirectTo:'/home',
+    pathMatch:'full',
+  },
+  
+  {
+    path:'',
+    children: [
+      {
+        path:'home',
+        loadChildren: () => import('./pages/home/home.module').then(function (m) {
+            return m.HomeModule;
+          }),
+      },
+    ],
+  },
+
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TuMotor/src/app/app.module.ts
+++ TuMotor/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
 import { ServicioComponent } from './pages/servicio/servicio.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PagesComponent,
     ServicioComponent,
+    NavbarComponent,
+    HomeComponent,
+    LandingComponent,
   ],
   imports: [
     BrowserModule,
